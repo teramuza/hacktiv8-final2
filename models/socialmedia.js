d45ab82e3e89c5cf08c3834b783fa702dd1241f7
Teuku Raja <teramuza@gmail.com>
2021-12-14 19:24:54 +0700

    add model & migration file

---
+++ models/socialmedia.js
@@ -0,0 +1,29 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class SocialMedia extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  SocialMedia.init({
+    name: DataTypes.STRING,
+    social_media_url: DataTypes.STRING,
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: 'Users',
+      referencesKey: 'id'
+    }
+  }, {
+    sequelize,
+    modelName: 'SocialMedia',
+  });
+  return SocialMedia;
+};
--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 'use strict';
 const {Model} = require('sequelize');
+const { SocialMedia } = require('.');
 
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
@@ -100,5 +101,7 @@ module.exports = (sequelize, DataTypes) => {
     sequelize,
     modelName: 'User',
   });
+  User.hasOne(SocialMedia, {foreignKey: 'user_id', foreignKeyConstraint: true})
+
   return User;
 };
