466807189ad33aab1cf51e39d92f90001388f24b
Teuku Raja <teramuza@gmail.com>
2021-12-16 23:49:32 +0700

    move hash password as a hooks function

--- controllers/users.js
+++ controllers/users.js
@@ -20,22 +20,20 @@ const register = async (req, res) => {
             age,
             phone_number
         } = req.body;
-        await bcrypt.hash(password, 10, (err, encrypt) => {
-            User.create({full_name, email, username, password: encrypt, profile_image_url, age, phone_number})
-                .then(() => responseUtil.successResponse(
-                        res,
-                        `Hi ${full_name}, your account was created successfully`,
-                        {user: {email, full_name, username, profile_image_url, age, phone_number}},
-                        201
-                    ))
-                .catch((e) => {
-                    if (e instanceof ValidationError)
-                        return responseUtil.validationErrorResponse(res, e.errors[0].message)
-                    else {
-                        return responseUtil.badRequestResponse(res, e);
-                    }
-                });
-        })
+        User.create({full_name, email, username, password, profile_image_url, age, phone_number})
+            .then(() => responseUtil.successResponse(
+                res,
+                `Hi ${full_name}, your account was created successfully`,
+                {user: {email, full_name, username, profile_image_url, age, phone_number}},
+                201
+            ))
+            .catch((e) => {
+                if (e instanceof ValidationError)
+                    return responseUtil.validationErrorResponse(res, e.errors[0].message)
+                else {
+                    return responseUtil.badRequestResponse(res, e);
+                }
+            });
     } catch (e) {
         return responseUtil.serverErrorResponse(res, e.message);
     }
--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 'use strict';
 const {Model} = require('sequelize');
+const bcrypt = require("bcrypt");
 
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
@@ -99,6 +100,17 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'User',
+    hooks: {
+      beforeCreate: (user, _) => {
+        return bcrypt.hash(user.password, 10)
+            .then(hash => {
+              user.password = hash;
+            })
+            .catch(err => {
+              throw new Error(err);
+            })
+      }
+    },
   });
 
   return User;
